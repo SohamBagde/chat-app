afd082ecaf936fc68ec4a597ecd38a618b3ad43c
SohamBagde <108925539+SohamBagde@users.noreply.github.com>
2024-02-17 16:03:23 +0530

    Initial commit

---
+++ src/main/java/com/project/chatapp/user/Status.java
@@ -0,0 +1,5 @@
+package com.project.chatapp.user;
+
+public enum Status {
+    ONLINE,OFFLINE
+}
---
+++ src/main/java/com/project/chatapp/user/UserRepository.java
@@ -0,0 +1,10 @@
+package com.project.chatapp.user;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+
+public interface UserRepository extends MongoRepository<User, String> {
+List<User> findAllByStatus(Status status);
+}
